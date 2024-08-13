e214724cdd2617d4847964fc60451f563a3ea43a
Maksim Burtsev <maks.artmovich@bk.ru>
2024-08-13 22:46:21 +0300

    Search problem

--- src/storage.js
+++ src/storage.js
@@ -1,62 +1,61 @@
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
+import path from 'path'
+import fs from 'fs'
 
+function getAbsoluteFilePath(filepath) {
+	return path.resolve(process.cwd(), filepath)
+}
 function readFile(filename) {
-  const newFileName = filename.split('/').reverse()[0];
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirName = path.dirname(__filename);
-  const fullPath = path.join(__dirName, '..', '__fixtures__', newFileName);
-  return fs.readFileSync(fullPath, 'utf-8');
+	return fs.readFileSync(getAbsoluteFilePath(filename), 'utf-8')
 }
 
 function getExtensions(filepath) {
-  const [, extend] = filepath.split('.');
-  return extend;
+	return path.extname(filepath).substring(1)
 }
 
 function getDifference(data1, data2) {
-  const keys = Object.keys({ ...data1, ...data2 }).sort((a, b) => a.localeCompare(b));
+	const keys = Object.keys({ ...data1, ...data2 }).sort((a, b) =>
+		a.localeCompare(b)
+	)
 
-  const result = keys.map((key) => {
-    const oldValue = data1[key];
-    const newValue = data2[key];
-    if (!Object.hasOwn(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        newValue,
-      };
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return {
-        type: 'deleted',
-        key,
-        oldValue,
-      };
-    }
-    if (typeof oldValue === 'object' && typeof newValue === 'object') {
-      return {
-        type: 'nested',
-        key,
-        children: getDifference(oldValue, newValue),
-      };
-    }
-    if (oldValue !== newValue) {
-      return {
-        type: 'changed',
-        key,
-        oldValue,
-        newValue,
-      };
-    }
-    return {
-      type: 'unchanged',
-      key,
-      oldValue,
-    };
-  });
+	const result = keys.map(key => {
+		const oldValue = data1[key]
+		const newValue = data2[key]
+		if (!Object.hasOwn(data1, key)) {
+			return {
+				type: 'added',
+				key,
+				newValue,
+			}
+		}
+		if (!Object.hasOwn(data2, key)) {
+			return {
+				type: 'deleted',
+				key,
+				oldValue,
+			}
+		}
+		if (typeof oldValue === 'object' && typeof newValue === 'object') {
+			return {
+				type: 'nested',
+				key,
+				children: getDifference(oldValue, newValue),
+			}
+		}
+		if (oldValue !== newValue) {
+			return {
+				type: 'changed',
+				key,
+				oldValue,
+				newValue,
+			}
+		}
+		return {
+			type: 'unchanged',
+			key,
+			oldValue,
+		}
+	})
 
-  return result;
+	return result
 }
-export { getDifference, readFile, getExtensions };
+export { getDifference, readFile, getExtensions }
