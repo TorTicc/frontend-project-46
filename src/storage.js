4c9033b2a037ca2fe7c1a7f87542d2361382d84d
Maksim Burtsev <maks.artmovich@bk.ru>
2024-08-14 22:29:12 +0300

    321

--- src/storage.js
+++ src/storage.js
@@ -15,7 +15,7 @@ function getExtensions(filepath) {
 function getDifference(data1, data2) {
   const keys = Object.keys({ ...data1, ...data2 });
 
-  const sortKeys = keys.toSorted((a, b) => a.localeCompare(b));
+  const sortKeys = keys.slice(0).sort((a, b) => a.localeCompare(b));
 
   const result = sortKeys.map((key) => {
     const oldValue = data1[key];
